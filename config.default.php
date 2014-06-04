19ac11c9ce8ea436a37f3a57ee913f57b40f8faf
Jack Lu <jacklu@entgaming.net>
2014-06-04 11:19:05 -0400

    Architectural change: remove master, instead use MySQL as task queue.
    Each monitoring server runs one or more monitor.php instances. monitor.php continuously loops at the database to check if there are any checks which need immediate execution. We use a MySQL-based locking system to prevent multiple instances from interacting with a check at the same time; to handle monitoring node failures, locks timeout after a certain interval.

--- config.default.php
+++ config.default.php
@@ -17,15 +17,9 @@ $config['mail_smtp_password'] = 'password';
 
 //delimiter to use for check data
 $config['check_delimiter'] = '|';
+$config['alert_delimiter'] = '|';
 
-//multi settings
-// multi mode allows you to iterate over slaves when running checks
-// currently the only purpose is to avoid sending erroneous alerts in case one slave dies
-$config['multi_master'] = '1.2.3.4'; //IP that slaves should accept connections from
-$config['multi_slaves'] = array('5.6.7.8', 'localhost'); //slave hosts
-$config['multi_port'] = 19549; //port to communicate with slaves
-$config['multi_contact'] = 'admin@example.com'; //email to alert if a slave fails
-$config['multi_secret'] = 'CHANGEMEPLEASE'; //secret to authenticate master with slave (in addition to IP)
-$config['multi_interval'] = 30; //interval to execute checks
+//daemon settings
+$config['sleep_interval'] = 5; //seconds to sleep when idle
 
 ?>
--- monitor.php
+++ monitor.php
@@ -6,84 +6,99 @@ require_once('database.php');
 require_once('checks.php');
 require_once('alerts.php');
 
-monitor_lock_init('monitor.php');
+$uid = uid(16);
+$last_cleanup = 0;
 
-$result = database_query("SELECT id, name, type, data, fail_count, success_count, status, last_alert, turn_count FROM checks", array(), true);
-
-while($row = $result->fetch()) {
-	$check_id = $row['id'];
-	$check_name = $row['name'];
-	$fail_count = $row['fail_count'];
-	$success_count = $row['success_count'];
-	$status = $row['status'];
-	$last_alert = $row['last_alert'];
-	$turn_count = $row['turn_count'];
-
-	$check_fname = "check_{$row['type']}";
-
-	if(!function_exists($check_fname)) {
-		die("Invalid check function [{$check_fname}]!\n");
+while(true) {
+	if(time() - $last_cleanup >= 60) {
+		database_query("UPDATE checks SET `lock` = '' WHERE TIMESTAMPDIFF(SECOND, last_locked, NOW()) >= 60");
+		$last_cleanup = time();
 	}
 
-	echo "processing check $check_id: calling $check_fname\n";
-	$check_result = call_user_func($check_fname, $row['data']);
+	database_query("UPDATE checks SET `lock` = ?, last_locked = NOW() WHERE `lock` = '' AND TIMESTAMPDIFF(SECOND, last_checked, NOW()) >= check_interval ORDER BY RAND() LIMIT 1", array($uid));
+	$result = database_query("SELECT id, name, type, data, fail_count, success_count, status, turn_count FROM checks WHERE `lock` = ? LIMIT 1", array($uid), true);
 
-	if(!is_array($check_result) || !isset($check_result['status'])) {
-		die("Bad check function [{$check_fname}]: returned non-array or missing status.\n");
-	} else if(!isset($check_result['message'])) {
-		$check_result['message'] = "Check offline: $check_name";
-	}
+	if($row = $result->fetch()) {
+		$check_id = $row['id'];
+		$check_name = $row['name'];
+		$fail_count = $row['fail_count'];
+		$success_count = $row['success_count'];
+		$status = $row['status'];
+		$turn_count = $row['turn_count'];
 
-	if($check_result['status'] == 'fail') {
-		echo "... got failure!\n";
-		$string_result = 'fail';
+		$check_fname = "check_{$row['type']}";
 
-		if($status == 'online') {
-			if($turn_count + 1 >= $fail_count) {
-				//target has failed
-				echo "... fail_count exceeded, target is offline state now\n";
-				database_query("UPDATE checks SET status = 'offline', turn_count = 0 WHERE id = ?", array($check_id));
+		if(!function_exists($check_fname)) {
+			die("Invalid check function [{$check_fname}]!\n");
+		}
 
-				$alert_result = database_query("SELECT contacts.id, contacts.type, contacts.data FROM contacts, alerts WHERE contacts.id = alerts.contact_id AND alerts.check_id = ?", array($check_id), true);
+		echo "processing check $check_id: calling $check_fname\n";
+		$check_result = call_user_func($check_fname, $row['data']);
 
-				while($alert_row = $alert_result->fetch()) {
-					echo "... alerting contact {$alert_row['id']}\n";
-					$alert_fname = "alert_{$alert_row['type']}";
+		if(!is_array($check_result) || !isset($check_result['status'])) {
+			die("Bad check function [{$check_fname}]: returned non-array or missing status.\n");
+		} else if(!isset($check_result['message'])) {
+			$check_result['message'] = "Check offline: $check_name";
+		}
 
-					if(!function_exists($alert_fname)) {
-						die("Invalid alert function [{$alert_fname}]!\n");
+		if($check_result['status'] == 'fail') {
+			echo "... got failure!\n";
+			$string_result = 'fail';
+
+			if($status == 'online') {
+				if($turn_count + 1 >= $fail_count) {
+					//target has failed
+					echo "... fail_count exceeded, target is offline state now\n";
+					$update_result = database_query("UPDATE checks SET status = 'offline', turn_count = 0, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
+
+					if($update_result->rowCount() == 1) {
+						//we still had the lock at the point where status was toggled to offline
+						//then, send the alert
+						$alert_result = database_query("SELECT contacts.id, contacts.type, contacts.data FROM contacts, alerts WHERE contacts.id = alerts.contact_id AND alerts.check_id = ?", array($check_id), true);
+
+						while($alert_row = $alert_result->fetch()) {
+							echo "... alerting contact {$alert_row['id']}\n";
+							$alert_fname = "alert_{$alert_row['type']}";
+
+							if(!function_exists($alert_fname)) {
+								die("Invalid alert function [{$alert_fname}]!\n");
+							}
+
+							call_user_func($alert_fname, $alert_row['data'], "Check offline: $check_name", $check_result['message']);
+						}
 					}
-
-					call_user_func($alert_fname, $alert_row['data'], "Check offline: $check_name", $check_result['message']);
+				} else {
+					//increase turn count
+					database_query("UPDATE checks SET turn_count = turn_count + 1, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
 				}
 			} else {
-				//increase turn count
-				database_query("UPDATE checks SET turn_count = turn_count + 1 WHERE id = ?", array($check_id));
+				database_query("UPDATE checks SET turn_count = 0, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
 			}
-		} else {
-			database_query("UPDATE checks SET turn_count = 0 WHERE id = ?", array($check_id));
-		}
-	} else if($check_result['status'] == 'success') {
-		echo "... got success\n";
-		$string_result = 'success';
+		} else if($check_result['status'] == 'success') {
+			echo "... got success\n";
+			$string_result = 'success';
 
-		if($status == 'offline') {;
-			if($turn_count + 1 >= $success_count) {
-				//target has come back online
-				echo "... success_count exceeded, target is online state now\n";
-				database_query("UPDATE checks SET status = 'online', turn_count = 0 WHERE id = ?", array($check_id));
+			if($status == 'offline') {;
+				if($turn_count + 1 >= $success_count) {
+					//target has come back online
+					echo "... success_count exceeded, target is online state now\n";
+					database_query("UPDATE checks SET status = 'online', turn_count = 0, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
+				} else {
+					//increase turn count
+					database_query("UPDATE checks SET turn_count = turn_count + 1, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
+				}
 			} else {
-				//increase turn count
-				database_query("UPDATE checks SET turn_count = turn_count + 1 WHERE id = ?", array($check_id));
+				database_query("UPDATE checks SET turn_count = 0, `lock` = '', last_checked = NOW() WHERE id = ? AND `lock` = ?", array($check_id, $uid));
 			}
 		} else {
-			database_query("UPDATE checks SET turn_count = 0 WHERE id = ?", array($check_id));
+			die("Check function [{$check_fname}] returned invalid status code [{$check_result['status']}]!\n");
 		}
-	} else {
-		die("Check function [{$check_fname}] returned invalid status code [{$check_result['status']}]!\n");
-	}
 
-	database_query("INSERT INTO check_results (check_id, result) VALUES (?, ?)", array($check_id, $check_result['status']));
+		database_query("INSERT INTO check_results (check_id, result) VALUES (?, ?)", array($check_id, $check_result['status']));
+	} else {
+		echo "no checks found, sleeping\n";
+		sleep($config['sleep_interval']);
+	}
 }
 
 ?>
